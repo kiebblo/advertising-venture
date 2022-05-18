c1412578d2bc95d102c896a75aa3aa7bec9b3ddc
kiebblo <kibarteta@gmail.com>
2022-05-18 19:14:28 +0200

    User setup and Auth configuration
    User Model, Controller and Route setup..

---
+++ server/model/User.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    lastname: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+}, { timestamps: true });
+
+userSchema.pre("save", function () {
+    try {
+        if (this.isModified("password")) {
+            this.password = bcrypt.hashSync(this.password, 10);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+const User =  mongoose.model("User", userSchema);
+module.exports = User;
