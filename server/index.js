7f3cdc3b3a452bd822bb696c86e476b650251f75
kiebblo <kibarteta@gmail.com>
2022-06-13 19:42:48 +0200

    User registration and connection
    User registration and connection via client side.
    Generation of coming pages.

--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@ const cors = require("cors")
 const express = require("express");
 const app = express();
 const UserRoute = require("./route/UserRoute");
-const AdvertisementController = require("./route/AdvertisementRoute");
-const ShoppingCartController = require("./route/ShoppingCartRoute");
+const AdvertisementRoute = require("./route/AdvertisementRoute");
+const ShoppingCartRoute = require("./route/ShoppingCartRoute");
 
 require("dotenv").config();
 require("./config/db");
@@ -11,8 +11,10 @@ require("./config/db");
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use("/api/user", UserRoute);
-app.use("/api/advertisement", AdvertisementController);
-app.use("/api/shoppingCart", ShoppingCartController);
+app.use("/api/advertisement", AdvertisementRoute);
+app.use("/api/shoppingCart", ShoppingCartRoute);
 app.use("/uploads", express.static(__dirname + "/uploads"));
+
 app.listen(process.env.PORT, () => console.log(`The server is running on port ${process.env.PORT}`));
